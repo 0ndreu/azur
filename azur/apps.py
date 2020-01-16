4887e32f685fb1f38a01873aa257b66d672769d8
0ndreu <andrey.artyushov@yandex.ru>
2020-01-16 20:35:42 +0300

    initialization and db

---
+++ azur/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AzurConfig(AppConfig):
+    name = 'azur'
---
+++ azur/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class WebPage (models.Model):
+    visited = models.DateTimeField()
+    tag = models.CharField(max_length=128)
+    url = models.TextField()
+    domain = models.TextField()
+    time_to_load = models.IntegerField()
+    load_code = models.IntegerField()
+    size = models.BigIntegerField()
+
+    class Meta:
+        verbose_name_plural = 'web pages'
+
+    def __str__(self):
+        return self.domain
+
